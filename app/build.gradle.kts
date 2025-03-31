ee65e3f220947caf587b3776bd8093471729b4b2
kristianconk <christiansoto6@gmail.com>
2025-03-31 00:10:29 -0600

    Añadir room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -56,6 +57,10 @@ dependencies {
     implementation(libs.androidx.material.icons.core.android)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.material.icons.extended.android)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
